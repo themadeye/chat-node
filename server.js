6a2e3a734e523fdb7ce0f0de3de63b9a18f3ca65
themadeye <leonkong42@gmail.com>
2020-10-17 15:39:18 +0800

    stage file

--- server.js
+++ server.js
@@ -37,5 +37,6 @@ io.on('connection', socket => {
           }
      });
 });
+// connect to port
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server is running on ${PORT}`));
